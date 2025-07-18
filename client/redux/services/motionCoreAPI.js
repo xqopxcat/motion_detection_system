224be9a757f11e154b385e07d31c1e4e0af12ddb
Steve Lee 李孟儒 <xqopxcat@gmail.com>
2025-07-18 14:30:38 +0800

    add env and gitignore in client

--- client/redux/services/motionCoreAPI.js
+++ client/redux/services/motionCoreAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const motionCoreApi = createApi({
     reducerPath: 'motionCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8080/api/',
+        baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api/`,
     }),
     tagTypes: ['Motion'],
     endpoints: (builder) => ({
